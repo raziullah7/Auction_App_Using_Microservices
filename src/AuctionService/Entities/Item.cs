27af68173a6c555292d42dd4eb94677e9a0df38f
Razi Ullah <raziullah913@gmail.com>
2024-06-16 15:59:22 +0500

    => made all the entities => made nav properties to connect Item.cs to Auction.cs

---
+++ src/AuctionService/Entities/Item.cs
@@ -0,0 +1,19 @@
+﻿namespace AuctionService.Entities
+{
+    public class Item
+    {
+        public Guid Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        public int Mileage { get; set; }
+        public string ImageUrl { get; set; }
+
+        // nav properties to establish one-to-one relationship with Auction.cs
+        // using the conventional method (which is nav properties)
+        public Auction Auction { get; set; }
+        public Guid AuctionId { get; set; }
+
+    }
+}
