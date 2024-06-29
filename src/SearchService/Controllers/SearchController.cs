1514d41d93718a282a75f37d20c13dbf64012a83
Razi Ullah <raziullah913@gmail.com>
2024-06-29 13:35:45 +0500

    => added seed data to SearchAuction Service => made DbInitializer.cs for seeding data in the DB => added a SearchController.cs and made a GET request

---
+++ src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Controllers;
+
+[ApiController]
+[Route("api/search")]
+public class SearchController : ControllerBase
+{
+    public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm)
+    {
+        // finding all "Item"s from DB
+        var query = DB.Find<Item>();
+        // sorting using Make property in Ascending order
+        query.Sort(x => x.Ascending(a => a.Make));
+
+        // if the searchTerm is NOT null
+        if (!string.IsNullOrEmpty(searchTerm))
+        {
+            // do a full text search and sort using the meta-score of each text result
+            query.Match(Search.Full, searchTerm).SortByTextScore();
+        }
+        
+        // execute query
+        var result = await query.ExecuteAsync();
+        // return the list of items
+        return result;
+    }
+}
---
+++ src/SearchService/Data/DbInitializer.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json;
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Data;
+
+public class DbInitializer
+{
+    public static async Task InitDb(WebApplication app)
+    {
+        // static call for MongoDb
+        await DB.InitAsync("SearchDb", MongoClientSettings
+            .FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
+        
+        // indexing for search
+        await DB.Index<Item>()
+            .Key(x => x.Make, KeyType.Text)
+            .Key(x => x.Model, KeyType.Text)
+            .Key(x => x.Color, KeyType.Text)
+            .CreateAsync();
+        
+        // getting the count of items from Mongo DB
+        var count = await DB.CountAsync<Item>();
+
+        if (count == 0)
+        {
+            Console.WriteLine("No data - will attempt to seed");
+            
+            // getting data related to items
+            var itemData = await File.ReadAllTextAsync("Data/auctions.json");
+            
+            // making options
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true};
+            
+            // passing itemData and options to get all data
+            var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+            
+            // saving changes
+            await DB.SaveAsync(items);
+        }
+    }
+}
