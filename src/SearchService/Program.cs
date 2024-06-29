f481eeb7d10ce1a826ac2598b6d9e625611a26e7
Razi Ullah <raziullah913@gmail.com>
2024-06-29 13:35:57 +0500

    => added seed data to SearchAuction Service => made DbInitializer.cs for seeding data in the DB => added a SearchController.cs and made a GET request

---
+++ src/SearchService/Program.cs
@@ -0,0 +1,28 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Data;
+using SearchService.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+    await DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+
+app.Run();
