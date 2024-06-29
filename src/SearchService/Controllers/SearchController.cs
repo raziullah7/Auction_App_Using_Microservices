78e38b5888764fec54877b942b136d959d7aa64a
Razi Ullah <raziullah913@gmail.com>
2024-06-29 17:27:54 +0500

    => implemented sorting, ordering, filtering and null checks for the GET in SearchController.cs => also added SearchParams.cs to bundle all the searching arguments together

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.RequestHelpers;
 
 namespace SearchService.Controllers;
 
@@ -8,23 +9,59 @@ namespace SearchService.Controllers;
 [Route("api/search")]
 public class SearchController : ControllerBase
 {
-    public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm)
+    [HttpGet]
+    public async Task<ActionResult<List<Item>>> SearchItems([FromQuery]SearchParams searchParams)
     {
-        // finding all "Item"s from DB
-        var query = DB.Find<Item>();
-        // sorting using Make property in Ascending order
-        query.Sort(x => x.Ascending(a => a.Make));
+        // finding all "Item"s from DB using paged search
+        var query = DB.PagedSearch<Item, Item>();
 
         // if the searchTerm is NOT null
-        if (!string.IsNullOrEmpty(searchTerm))
+        if (!string.IsNullOrEmpty(searchParams.SearchTerm))
         {
             // do a full text search and sort using the meta-score of each text result
-            query.Match(Search.Full, searchTerm).SortByTextScore();
+            query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
         }
+        
+        // sorting
+        query = searchParams.OrderBy switch
+        {
+            "make" => query.Sort(x => x.Ascending(a => a.Make)),
+            "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
+            _ => query.Sort(x => x.Ascending(a => a.AuctionEnd))
+            // underscore means default options
+        };
+        
+        // filtering options
+        query = searchParams.FilterBy switch
+        {
+            "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+            "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6) 
+                                             && x.AuctionEnd < DateTime.UtcNow),
+            _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
+        };
+
+        // checking for Seller and Winner
+        if (!string.IsNullOrEmpty(searchParams.Seller))
+        {
+            query.Match(x => x.Seller == searchParams.Seller);
+        }
+        if (!string.IsNullOrEmpty(searchParams.Winner))
+        {
+            query.Match(x => x.Winner == searchParams.Winner);
+        }
+        
+        // also getting the page number and size number
+        query.PageNumber(searchParams.PageNumber);
+        query.PageSize(searchParams.PageSize);
         
         // execute query
         var result = await query.ExecuteAsync();
-        // return the list of items
-        return result;
+        // return by implementing pagination
+        return Ok(new
+        {
+            results = result.Results,
+            pageCount = result.PageCount,
+            totalCount = result.TotalCount
+        });
     }
 }
