2c660688debef51200bde4a1e5f02c515090277e
Razi Ullah <50240668+raziullah7@users.noreply.github.com>
2024-08-05 22:53:00 +0500

    => added NEXT JS client: did a bunch of development and styling in it

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import React from "react";
+import AuctionCard from "./AuctionCard";
+import { Auction, PagedResult } from "@/types";
 
-async function getData() {
-    const res = await fetch("http://localhost:6001/search");
+async function getData(): Promise<PagedResult<Auction>> {
+  const res = await fetch("http://localhost:6001/search?pageSize=10");
 
-    if (!res.ok) throw new Error("Failed to fetch data");
+  if (!res.ok) throw new Error("Failed to fetch data");
 
-    return res.json();
+  return res.json();
 }
 
 export default async function Listings() {
-    const data = await getData();
+  const data = await getData();
 
-    return (
-        <div>
-            {JSON.stringify(data, null, 2)}
-        </div>
-    );
+  return (
+    <div className="grid grid-cols-4 gap-6">
+      {data &&
+        data.results.map((auction) => (
+          <AuctionCard auction={auction} key={auction.id} />
+        ))}
+    </div>
+  );
 }
--- frontend/web-app/next.config.mjs
+++ frontend/web-app/next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    // images.domains got deprecated
+    // domains: ["cdn.pixabay.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.pixabay.com",
+      },
+    ],
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+};
 
 export default nextConfig;
